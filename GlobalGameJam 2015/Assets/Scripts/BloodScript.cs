8e6bdabfa7e95a5ef0c3bafba0ec3be95521eabc
Benjamin Bourot <bourot.benjamin@gmail.com>
2015-01-25 11:46:05 +0100

    Fix blood

--- GlobalGameJam 2015/Assets/Scripts/BloodScript.cs
+++ GlobalGameJam 2015/Assets/Scripts/BloodScript.cs
@@ -7,35 +7,12 @@ public class BloodScript : MonoBehaviour {
     Material[] bloodMaterials;
     [SerializeField]
     Object bloodPrefab;
-    GameObject[] bloodTransforms;
-    private int lastBlood;
-
-	// Use this for initialization
-	void Start () {
-        lastBlood = -1;
-        bloodTransforms = new GameObject[200];
-	    for(int i = 0; i < 200; i++)
-        {
-            GameObject tmpBlod = (GameObject)GameObject.Instantiate(bloodPrefab);
-            tmpBlod.renderer.material = bloodMaterials[Random.Range(0, bloodMaterials.Length)];
-            bloodTransforms[i] = tmpBlod;
-        }
-	}
 
     public void showNextBlood(Vector3 pos)
     {
-        if(lastBlood < 199)
-        {
-            lastBlood = lastBlood + 1;
-        }
-        else
-        {
-            lastBlood = 0;
-        }
-        Color c = bloodTransforms[lastBlood].renderer.material.color;
-        bloodTransforms[lastBlood].transform.position = pos + transform.forward;
-        c.a = 1;
-        bloodTransforms[lastBlood].renderer.material.color = c;
+        GameObject tmpBlod = (GameObject)GameObject.Instantiate(bloodPrefab);
+        tmpBlod.renderer.material = bloodMaterials[Random.Range(0, bloodMaterials.Length)];
+        tmpBlod.transform.position = pos;
     }
 
 
