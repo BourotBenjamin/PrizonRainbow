72ff274b43afceae3c7e37e1587e83bb9b6f7872
Yquem <supermollarman@yahoo.fr>
2015-01-25 11:24:24 +0100

    Light

---
+++ GlobalGameJam 2015/Assets/Scripts/LightCLignote.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightCLignote : MonoBehaviour {
+
+	private Light2D Mylight;
+	bool WhichColor;
+
+	void Start()
+	{
+		Mylight = GetComponent<Light2D>();
+	}
+	// Update is called once per frame
+	void Update () {
+		Mylight.LightColor = Color.Lerp(Color.black,Color.red,Mathf.Abs( Mathf.Sin(Time.time*3f)));
+		Color TROLOLO = Mylight.LightColor;
+		TROLOLO.a = 0;
+		Mylight.LightColor = TROLOLO;
+	}
+}
---
+++ GlobalGameJam 2015/Assets/Scripts/TRIGGERDOORWAIT.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TRIGGERDOORWAIT : MonoBehaviour {
+	public AudioSource ALARM;
+	public GameObject DoorDelete;
+	public GameObject light;
+
+
+	void OnTriggerEnter()
+	{
+		ALARM.PlayOneShot(ALARM.clip);
+		Invoke("DeleteDoor", 10f);
+		light.SetActive (true);
+	}
+	void DeleteDoor()
+	{
+		DoorDelete.SetActive(false);
+	}
+}
